22aa6de8e012f63540280f8a37bef47b2421595b
Krutikap12 <krupatil30@outlook.com>
2024-10-17 13:16:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exeption_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st no:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd no:");
+            int num2 = int.Parse(Console.ReadLine());
+            int result = 0;
+            
+            try
+            {
+                result = num1 % num2;
+                Console.WriteLine(result);
+                if (num2 ==0)
+                {
+                    throw new DivideByZeroException();
+                }
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
